397ef90828bba31964f7b1716846261929290628
Jieun Kim <jisilver.kim@gmail.com>
2020-07-26 00:49:01 +0900

    Update Hooks code

--- 4.반응속도체크/ResponseCheck.jsx
+++ 4.반응속도체크/ResponseCheck.jsx
@@ -1,34 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState, useRef } from 'react';
 
-class ResponseCheck extends Component {
-    state = {
-        state: 'waiting',
-        message: '클릭해서 시작하세요.',
-        result: [],
+const ResponseCheck = () => {
+    const [state, setState] = useState('waiting');
+    const [message, setMessage] = useState('클릭해서 시작하세요.');
+    const [result, setResult] = useState([]);
+    const timeout = useRef(null);
+    const startTime = useRef();
+    const endTime = useRef();
+
+    const onClickScreen = () => {
+        if (state === 'waiting') {
+            setState('ready');
+            setMessage('초록색이 되면 클릭하세요.');
+            timeout.current = setTimeout(() => {
+                setState('now');
+                setMessage('지금 클릭');
+                startTime.current = new Date();
+            }, Math.floor(Math.random() * 1000) + 2000);
+        } else if (state === 'ready') {
+            clearTimeout(timeout.current);
+            setState('waiting');
+            setMessage('너무 성급하시군요! 초록색이 된 후에 클릭하세요.');
+        } else if (state === 'now') {
+            endTime.current = new Date();
+            setState('waiting');
+            setMessage('클릭해서 시작하세요.');
+            setResult(prevResult => {
+                return [...prevResult, endTime.current - startTime.current];
+            });
+        }
     };
 
-    onClickScreen = () => {
-
+    const onReset = () => {
+        setResult([]);
     };
 
-    renderAverage = () => {
-        const { result } = this.state;
+    const renderAverage = () => {
         return result.length === 0
-            ? null // jsx에서 false, undefined, null은 태그 없음을 의미. 처음엔 null, result에 값이 있으면 랜더링
-            : <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
-    }
-
-    render() {
-        const { state, message } = this.state;
-        return (
-            <>
-                <div id="screen" className={state} onClick={this.onClickScreen}>
-                    {message}
-                </div>
-                {this.renderAverage()}
+            ? null
+            : <>
+                <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+                <button onClick={onReset}>리셋</button>
             </>
-        );
-    }
-}
+    };
+
+    return (
+        <>
+            <div id="screen" className={state} onClick={onClickScreen}>
+                {message}
+            </div>
+            {renderAverage()}
+        </>
+    );
+};
 
 export default ResponseCheck;
