90bee15af9d2f73412a02aae6d4f0a519ef29f6c
Jieun Kim <jisilver.kim@gmail.com>
2020-08-14 02:25:26 +0900

    FIx: turn delete

--- 7.틱택토/Td.jsx
+++ 7.틱택토/Td.jsx
@@ -1,12 +1,14 @@
 import React, { useCallback } from 'react';
-import { CLICK_CELL, CHANGE_TURN } from './TicTacToe';
+import { CLICK_CELL } from './TicTacToe';
 
 const Td = ({ rowIndex, cellIndex, dispatch, cellData }) => {
     const onClickTd = useCallback(() => {
         console.log(rowIndex, cellIndex);
-        dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex }); // 칸 클릭 후
-        dispatch({ type: CHANGE_TURN }); // 턴 바꾸기
-    }, []);
+        if (cellData) { // 기존 데이터가 있으면 리턴
+            return;
+        }
+        dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex });
+    }, [cellData]);
 
     return (
         <td onClick={onClickTd}>{cellData}</td>
