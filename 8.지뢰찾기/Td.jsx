dcb165a7d3e471bb5af141b6f08f19f28f97ca13
jisilver <jisilver.kim@gmail.com>
2020-08-22 02:31:06 +0900

    Add onContextMenu event

--- 8.지뢰찾기/Td.jsx
+++ 8.지뢰찾기/Td.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { CODE, TableContext } from './MineSearch';
+import React, { useContext, useCallback } from 'react';
+import { CLICK_MINE, CODE, FLAG_CELL, NORMALIZE_CELL, OPEN_CELL, QUESTION_CELL, TableContext } from './MineSearch';
 
 const getTdStyle = (code) => {
     switch (code) {
@@ -8,13 +8,24 @@ const getTdStyle = (code) => {
             return {
                 background: '#444',
             };
+        case CODE.CLICKED_MINE:
         case CODE.OPENED:
             return {
-                background: 'white'
+                background: 'white',
+            };
+        case CODE.QUESTION_MINE:
+        case CODE.QUESTION:
+            return {
+                background: 'yellow',
+            };
+        case CODE.FLAG_MINE:
+        case CODE.FLAG:
+            return {
+                background: 'red',
             };
         default:
             return {
-                background: 'white'
+                background: 'white',
             };
     }
 };
@@ -25,16 +36,72 @@ const getTdText = (code) => {
             return '';
         case CODE.MINE:
             return 'X';
+        case CODE.CLICKED_MINE:
+            return '펑';
+        case CODE.FLAG_MINE:
+        case CODE.FLAG:
+            return '!';
+        case CODE.QUESTION_MINE:
+        case CODE.QUESTION:
+            return '?';
         default:
             return '';
     }
 };
 
 const Td = ({ rowIndex, cellIndex }) => {
-    const { tableData } = useContext(TableContext);
+    const { tableData, dispatch, halted } = useContext(TableContext);
+
+    const onClickTd = useCallback(() => {
+        if (halted) {
+            return;
+        }
+        switch (tableData[rowIndex][cellIndex]) {
+            case CODE.OPENED:
+            case CODE.FLAG_MINE:
+            case CODE.FLAG:
+            case CODE.QUESTION_MINE:
+            case CODE.QUESTION:
+                return;
+            case CODE.NORMAL:
+                dispatch({ type: OPEN_CELL, row: rowIndex, cell: cellIndex });
+                return;
+            case CODE.MINE:
+                dispatch({ type: CLICK_MINE, row: rowIndex, cell: cellIndex });
+                return;
+            default:
+                return;
+        }
+    }, [tableData[rowIndex][cellIndex], halted]);
+
+    const onRightClickTd = useCallback((e) => {
+        e.preventDefault(); // 우클릭 시 나오는 메뉴 안 뜨게 하기
+        if (halted) {
+            return;
+        }
+        switch (tableData[rowIndex][cellIndex]) {
+            case CODE.NORMAL:
+            case CODE.MINE:
+                dispatch({ type: FLAG_CELL, row: rowIndex, cell: cellIndex });
+                return;
+            case CODE.FLAG_MINE:
+            case CODE.FLAG:
+                dispatch({ type: QUESTION_CELL, row: rowIndex, cell: cellIndex });
+                return;
+            case CODE.QUESTION_MINE:
+            case CODE.QUESTION:
+                dispatch({ type: NORMALIZE_CELL, row: rowIndex, cell: cellIndex });
+                return;
+            default:
+                return;
+        }
+    }, [tableData[rowIndex][cellIndex], halted]);
+
     return (
         <td
             style={getTdStyle(tableData[rowIndex][cellIndex])}
+            onClick={onClickTd}
+            onContextMenu={onRightClickTd} // 우클릭 이벤트
         >{getTdText(tableData[rowIndex][cellIndex])}</td>
     )
 };
