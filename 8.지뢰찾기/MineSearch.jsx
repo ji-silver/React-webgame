e6afac4f3416bc0c7dc951b7204d63ada6551edc
jisilver <jisilver.kim@gmail.com>
2020-08-27 02:04:02 +0900

    Add recursive function

--- 8.지뢰찾기/MineSearch.jsx
+++ 8.지뢰찾기/MineSearch.jsx
@@ -72,29 +72,69 @@ const reducer = (state, action) => {
             };
         case OPEN_CELL: {
             const tableData = [...state.tableData];
-            tableData[action.row] = [...state.tableData[action.row]];
-            let around = [];
-            if (tableData[action.row - 1]) { // 클릭한 칸 기준으로 윗줄이 있으면
+            // tableData[action.row] = [...state.tableData[action.row]];
+            tableData.forEach((row, i) => { // 클릭한 칸 뿐 아니라 모든 칸을 새로운 객체로 만들기
+                tableData[i] = [...state.tableData[i]];
+            });
+            const checked = [];
+            const checkAround = (row, cell) => {
+                // 주변 지뢰 갯수 찾기
+                if ([CODE.OPENED, CODE.FLAG_MINE, CODE.FLAG, CODE.QUESTION_MINE, CODE.QUESTION].includes(tableData[row][cell])) {
+                    return;
+                } // 상하좌우 없는 칸은 안 열기
+                if (row < 0 || row >= tableData.length || cell < 0 || cell >= tableData[0].length) {
+                    return;
+                } // 닫힌 칸만 열기
+                if (checked.includes(row + ',' + cell)) {
+                    return;
+                } else {
+                    checked.push(row + ',' + cell);
+                } // 한 번 연 칸은 무시하기
+                let around = [];
+                if (tableData[row - 1]) {
+                    around = around.concat(
+                        tableData[row - 1][cell - 1],
+                        tableData[row - 1][cell],
+                        tableData[row - 1][cell + 1]
+                    );
+                }
                 around = around.concat(
-                    tableData[action.row - 1][action.cell - 1],
-                    tableData[action.row - 1][action.cell],
-                    tableData[action.row - 1][action.cell + 1]
+                    tableData[row][cell - 1],
+                    tableData[row][cell + 1]
                 );
-            }
-            around = around.concat(
-                tableData[action.row][action.cell - 1],
-                tableData[action.row][action.cell + 1]
-            );
-            if (tableData[action.row + 1]) { // 클릭한 칸 기준으로 밑에줄이 있으면
-                around = around.concat(
-                    tableData[action.row + 1][action.cell - 1],
-                    tableData[action.row + 1][action.cell],
-                    tableData[action.row + 1][action.cell + 1]
-                );
-            }
-            // 주변 지뢰 갯수 찾기
-            const count = around.filter((v) => [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v)).length;
-            tableData[action.row][action.cell] = count;
+                if (tableData[row + 1]) {
+                    around = around.concat(
+                        tableData[row + 1][cell - 1],
+                        tableData[row + 1][cell],
+                        tableData[row + 1][cell + 1]
+                    );
+                }
+                const count = around.filter((v) => [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v)).length;
+                tableData[row][cell] = count;
+
+                // 지뢰가 없으면 주변 8칸 열기
+                if (count === 0) {
+                    const near = [];
+                    if (row - 1 > -1) {
+                        near.push([row - 1, cell - 1]);
+                        near.push([row - 1, cell]);
+                        near.push([row - 1, cell + 1]);
+                    }
+                    near.push([row, cell - 1]);
+                    near.push([row, cell + 1]);
+                    if (row + 1 < tableData.length) {
+                        near.push([row + 1, cell - 1]);
+                        near.push([row + 1, cell]);
+                        near.push([row + 1, cell + 1]);
+                    }
+                    near.forEach((n) => {
+                        if (tableData[n[0]][n[1]] !== CODE.OPENE) {
+                            checkAround(n[0], n[1]);
+                        }
+                    })
+                }
+            };
+            checkAround(action.row, action.cell);
             return {
                 ...state,
                 tableData,
